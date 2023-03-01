8e800d0f35138400310ab063c207836860c30a0f
Rodrigo Cerqueira <rodrigocerqueira@outlook.com>
2023-02-28 21:44:50 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dsfolder/DsfolderApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsfolderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsfolder/DsfolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsfolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsfolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
